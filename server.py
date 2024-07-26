d1b1ffc0af8023453af6e6132f8693704d2d3b66
sumedh133 <sumedh133@gmail.com>
2024-07-26 18:46:05 +0530

    landing page integrated

--- server.py
+++ server.py
@@ -59,7 +59,11 @@ def logout():
     
 @app.route("/")
 def home():
-    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    return render_template("index.html")
+
+@app.route('/aboutUs')
+def aboutUs():
+    return render_template('aboutUs.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=env.get("PORT", 3000))
---
+++ static/js/custom.js
@@ -0,0 +1,31 @@
+
+// nice select
+$(document).ready(function () {
+    $('select').niceSelect();
+});
+
+
+// owl carousel slider js
+$('.team_carousel').owlCarousel({
+    loop: true,
+    margin: 15,
+    dots: true,
+    autoplay: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            margin: 0
+        },
+        576: {
+            items: 2,
+        },
+        992: {
+            items: 3
+        }
+    }
+})
